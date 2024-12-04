5ccafb998144605d5d1c7be874f27c6f9346f40c
Apiraam Selvabaskaran <apiraam96@gmail.com>
2024-12-03 22:21:42 -0500

    feat: frontend routes to backend, backend uses .cjs notation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    fetch("/api/login")
+      .then((result) => result.json())
+      .then((data) => console.log(data.message));
+  }, []);
   return (
     <>
-      <h1>NyxBio</h1>
+      <h1 className="text-5xl font-bold">BenchFlow</h1>
     </>
   );
 }
